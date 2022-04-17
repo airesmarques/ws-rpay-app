6eef9e307c7e7f806e38c4af7f37dfe8dd3ea1e9
Aires Marques <34254546+airesmarques@users.noreply.github.com>
2022-04-17 21:18:48 +0100

    Updated file name

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
-      src="../images/washstation-icon.png"
+      src="../images/WashStation-icon.png"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
